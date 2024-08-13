5811488bbff426853e03079fa7c24d944ee27fdf
palapapa <palapapa93@gmail.com>
2024-08-13 23:54:13 +0800

    Added the keyboard hook
    Added the keyboard hook and temporarily made it output key press
    messages to the debug output.

--- Source/AppApplicationContext.cs
+++ Source/AppApplicationContext.cs
@@ -1,15 +1,51 @@
-﻿using System.Windows.Forms;
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
 using System.Diagnostics.CodeAnalysis;
+using static Windows.Win32.PInvoke;
+using System.Runtime.InteropServices;
 
 namespace Keysounds;
 
 /// <summary>
 /// The <see cref="ApplicationContext"/> used in this app. This is used to start the app without a <see cref="Form"/>.
 /// </summary>
-/// <param name="appNotifyIcon">The <see cref="IAppNotifyIcon"/> to inject.</param>
-internal class AppApplicationContext(IAppNotifyIcon appNotifyIcon) : ApplicationContext
+internal class AppApplicationContext : ApplicationContext
 {
-    [SuppressMessage("Performance", "CA1823:Avoid unused private fields", Justification = "This field is injected because its constructor has a side effect.")]
-    [SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "This field is injected because its constructor has a side effect.")]
-    private readonly IAppNotifyIcon appNotifyIcon = appNotifyIcon;
+    [SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "This field doesn't need to be read to function.")]
+    private readonly IAppNotifyIcon appNotifyIcon;
+
+    private readonly UnhookWindowsHookExSafeHandle LowLevelKeyboardHook;
+
+    public AppApplicationContext(IAppNotifyIcon appNotifyIcon)
+    {
+        this.appNotifyIcon = appNotifyIcon;
+        using Process currentProcess = Process.GetCurrentProcess();
+        // MainModule shouldn't be null because we are just getting the current module.
+        using ProcessModule currentModule = currentProcess.MainModule!;
+        // GetModuleHandle returns a FreeLibrarySafeHandle whose OwnsHandle is false, so its Dispose won't call FreeLibrary, which it shouldn't.
+        using FreeLibrarySafeHandle currentModuleHandle = GetModuleHandle(currentModule.ModuleName);
+        LowLevelKeyboardHook = SetWindowsHookEx(WINDOWS_HOOK_ID.WH_KEYBOARD_LL, LowLevelKeyboardProc, currentModuleHandle, 0);
+    }
+
+    private LRESULT LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+    {
+        if (nCode >= 0)
+        {
+            KBDLLHOOKSTRUCT keyInfo = Marshal.PtrToStructure<KBDLLHOOKSTRUCT>(lParam);
+            Debug.WriteLine(keyInfo.vkCode);
+        }
+        return CallNextHookEx(null, nCode, wParam, lParam);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (disposing)
+        {
+            LowLevelKeyboardHook.Dispose();
+        }
+    }
 }
