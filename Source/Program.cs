268bff187f8a08fb50f813e29e96b7fdfe38faf1
palapapa <palapapa93@gmail.com>
2024-08-14 16:38:01 +0800

    Added a message box that shows the message of an unhandled exception

--- Source/Program.cs
+++ Source/Program.cs
@@ -2,12 +2,14 @@ using System;
 using System.Windows.Forms;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Keysounds;
 
 internal static class Program
 {
     [STAThread]
+    [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "The topmost catch statement catches all unhandled exceptions to display a message box.")]
     private static void Main()
     {
         ApplicationConfiguration.Initialize();
@@ -15,6 +17,13 @@ internal static class Program
         builder.Services.AddSingleton<ApplicationContext, AppApplicationContext>()
             .AddSingleton<IAppNotifyIcon, AppNotifyIcon>();
         using IHost host = builder.Build();
-        Application.Run(host.Services.GetRequiredService<ApplicationContext>());
+        try
+        {
+            Application.Run(host.Services.GetRequiredService<ApplicationContext>());
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show(e.ToString(), "Keysounds: Unhandled Exception!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+        }
     }
 }
