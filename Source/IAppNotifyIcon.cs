643e0820ddb690b02df2f375d2e2ed2504fa919d
palapapa <palapapa93@gmail.com>
2024-08-15 13:22:46 +0800

    Corrected the documentation of IAppNotifyIcon and added JsonSchema
    The documentation of IAppNotifyIcon incorrectly referred to its member
    named NotifyIcon instead of the NotifyIcon class.

--- Source/IAppNotifyIcon.cs
+++ Source/IAppNotifyIcon.cs
@@ -4,7 +4,7 @@ using System.Windows.Forms;
 namespace Keysounds;
 
 /// <summary>
-/// Represents the <see cref="NotifyIcon"/> this app uses. Used for dependency injection purposes to wrap around a <see cref="NotifyIcon"/> since is doesn't implement an <see langword="interface"/>.
+/// Represents the <see cref="System.Windows.Forms.NotifyIcon"/> this app uses. Used for dependency injection purposes to wrap around a <see cref="System.Windows.Forms.NotifyIcon"/> since is doesn't implement an <see langword="interface"/>.
 /// </summary>
 internal interface IAppNotifyIcon : IDisposable
 {
